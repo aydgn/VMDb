9847c710cc70204180237bbf54e00abe9970ef14
Aydoğan <aydogan.gokberk@gmail.com>
2021-07-12 12:19:44 +0300

    sass & design files added

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <HeaderComp />
+  <h1>Hello!</h1>
 </template>
 
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import HeaderComp from "./components/HeaderComp.vue";
 
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+export default {
+  components: { HeaderComp },
+  setup() {},
+};
 </script>
 
 <style>
@@ -15,8 +17,5 @@ import HelloWorld from './components/HelloWorld.vue'
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
