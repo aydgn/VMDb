578e46296885e0aeec659a446fe466dbd617384c
Aydoğan Gökberk <aydogan.gokberk@gmail.com>
2021-07-17 15:21:00 +0300

    refactored movieTiles component

--- src/App.vue
+++ src/App.vue
@@ -2,61 +2,21 @@
   <HeaderComp />
 
   <main class="main container">
-    <h2>Trending TV Shows</h2>
-
-    <div class="movieTile">
-      <a
-        v-for="(show, index) in trendingTv"
-        :key="show.id"
-        :href="show.id"
-        class="movieTile__link"
-      >
-        <img
-          :src="`https://image.tmdb.org/t/p/w200${show.poster_path}`"
-          :alt="show.name"
-          :title="show.name"
-          class="movieTile__poster"
-        />
-        <span class="movieTile__name">{{ index + 1 }}. {{ show.name }}</span>
-      </a>
-    </div>
+    <MovieTiles title="Trending TV Shows" apiQuery="trending/tv/week" />
+    <MovieTiles title="Trending Movies" apiQuery="trending/movie/week" />
+    <Footer />
   </main>
-  <Footer />
 </template>
 
 <script>
-import { onMounted, ref } from "vue";
 import HeaderComp from "./components/shared/header/HeaderComp.vue";
 import Footer from "./components/shared/footer/Footer.vue";
+import MovieTiles from "./components/shared/MovieTiles.vue";
 
 export default {
-  components: { HeaderComp, Footer },
+  components: { HeaderComp, Footer, MovieTiles },
 
-  setup() {
-    let trendingTv = ref([""]);
-    const apikey = import.meta.env.VITE_KEY;
-
-    // fetch trending shows
-    async function fetchTrendingShows() {
-      await fetch(
-        `https://api.themoviedb.org/3/trending/tv/week?api_key=${apikey}`
-      )
-        .then(res => res.json())
-        .then(data => {
-          trendingTv.value = data.results;
-        })
-        .then(() => {
-          console.log(trendingTv);
-        })
-        .catch(err => {
-          console.log(err);
-        });
-    }
-    onMounted(fetchTrendingShows);
-    return {
-      trendingTv,
-    };
-  },
+  setup() {},
 };
 </script>
 
@@ -87,48 +47,6 @@ body {
 @media (min-width: 1400px) {
   .container {
     max-width: 1320px;
-  }
-}
-
-.movieTile {
-  display: flex;
-
-  flex-direction: row;
-  gap: 1rem;
-
-  padding: 1rem 0;
-  overflow-x: auto;
-  scroll-snap-type: x mandatory;
-  scrollbar-color: #404040 #5e5e5e;
-
-  &::-webkit-scrollbar {
-    height: 8px;
-  }
-
-  &::-webkit-scrollbar-thumb {
-    background-color: #404040;
-  }
-
-  &__link {
-    color: #fff;
-    text-decoration: none;
-    transition: all 50ms ease;
-    position: relative;
-
-    &:hover {
-      transform: scale(1.05);
-    }
-  }
-
-  &__name {
-    position: absolute;
-    top: 0px;
-    left: 0px;
-    background-color: rgba(#000, 0.2);
-    padding: 5px;
-    width: 100%;
-    text-align: center;
-    text-shadow: 1px 1px 0 #404040;
   }
 }
 </style>
---
+++ src/components/shared/MovieTiles.vue
@@ -0,0 +1,92 @@
+<template>
+  <h2>{{ title }}</h2>
+  <div class="movieTile">
+    <a
+      v-for="(data, index) in apiData"
+      :key="data.id"
+      :href="data.id"
+      class="movieTile__link"
+    >
+      <img
+        :src="`https://image.tmdb.org/t/p/w200${data.poster_path}`"
+        :alt="data.name"
+        :title="data.name"
+        class="movieTile__poster"
+      />
+      <span class="movieTile__name">{{ index + 1 }}. {{ data.name }}</span>
+    </a>
+  </div>
+</template>
+
+<script>
+import { onMounted, ref } from "vue";
+
+export default {
+  props: { title: String, apiQuery: String },
+  setup(props) {
+    const apiData = ref([]);
+    const apikey = import.meta.env.VITE_KEY;
+
+    const fetchApiData = async () => {
+      await fetch(
+        `https://api.themoviedb.org/3/${props.apiQuery}?api_key=${apikey}`
+      )
+        .then(res => res.json())
+        .then(data => {
+          apiData.value = data.results;
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    };
+    onMounted(fetchApiData);
+    return {
+      apiData,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.movieTile {
+  display: flex;
+
+  flex-direction: row;
+  gap: 1rem;
+
+  padding: 1rem 0;
+  overflow-x: auto;
+  scroll-snap-type: x mandatory;
+  scrollbar-color: #404040 #5e5e5e;
+
+  &::-webkit-scrollbar {
+    height: 8px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: #404040;
+  }
+
+  &__link {
+    color: #fff;
+    text-decoration: none;
+    transition: all 50ms ease;
+    position: relative;
+
+    &:hover {
+      transform: scale(1.05);
+    }
+  }
+
+  &__name {
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    background-color: rgba(#000, 0.2);
+    padding: 5px;
+    width: 100%;
+    text-align: center;
+    text-shadow: 1px 1px 0 #404040;
+  }
+}
+</style>
