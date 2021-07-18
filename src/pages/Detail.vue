cc9b0dcda38af05c75c2512b4d186002f71a65b8
Aydoğan Gökberk <aydogan.gokberk@gmail.com>
2021-07-18 17:51:49 +0300

    css row removed

--- src/pages/Detail.vue
+++ src/pages/Detail.vue
@@ -1,7 +1,7 @@
 <template>
   <section
     class="hero"
-    :style="`background-image: linear-gradient(90deg, rgba(0,0,0,1) 0%, rgba(0,0,0,0) 100%) ,url(https://image.tmdb.org/t/p/w1280${apiData.backdrop_path});`"
+    :style="`background-image: linear-gradient(90deg, rgba(0,0,0,1) 0%, rgba(0,0,0,0.2) 100%) ,url(https://image.tmdb.org/t/p/w1280${apiData.backdrop_path});`"
   >
     <div class="hero__info container">
       <div class="hero__poster">
@@ -18,7 +18,7 @@
             class="hero__image"
           />
         </a>
-        <div class="hero__row">
+        <div>
           <span class="hero__rating" alt="Rating" title="Rating">
             {{ apiData.vote_average }} / 10
           </span>
@@ -27,14 +27,16 @@
             {{ apiData.runtime }} min
           </span>
         </div>
-        <div class="hero__row">
-          <span
+
+        <div class="hero__genres">
+          <a
             class="hero__genre"
             v-for="genre in apiData.genres"
             :key="genre.id"
+            href="#"
           >
             {{ genre.name }}
-          </span>
+          </a>
         </div>
       </div>
       <div class="hero__details">
@@ -82,7 +84,7 @@ export default {
 .hero {
   position: relative;
   background-size: cover;
-  background-position: 50%;
+  background-position: center 0%;
   background-repeat: no-repeat;
   min-height: 650px;
   padding: 2rem 1rem;
@@ -113,30 +115,21 @@ export default {
     }
   }
 
-  &__row {
-    display: flex;
-    flex-direction: row;
-    gap: 0.5rem;
-    flex-wrap: wrap;
-    justify-content: center;
-    align-items: baseline;
-    margin: 0.5rem;
-
-    @include mq(tablet) {
-      justify-content: flex-end;
-    }
-  }
-
   &__image {
     box-shadow: 0 0 30px 1px #000;
     height: auto;
     width: 200px;
+    transition: all 50ms ease;
+    margin: 1rem;
+
+    &:hover {
+      transform: scale(1.1);
+    }
   }
   &__title {
     font-size: 2rem;
     margin: 0;
-    padding: 0;
-    text-shadow: #fff 1px 0 5px;
+    text-shadow: #fff 0 0 5px;
 
     @include mq(tablet) {
       font-size: 3rem;
@@ -147,14 +140,33 @@ export default {
     font-style: italic;
     margin: 0;
   }
+  &__genres {
+    margin: 1rem 0;
+    display: flex;
+    gap: 0.5rem;
+    flex-direction: row;
+    flex-wrap: wrap;
+  }
+
   &__genre {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
     background: $lightbg;
     padding: 0.5rem;
-    text-align: center;
+    color: #fff;
+    text-decoration: none;
+
+    &:hover {
+      background: $green;
+    }
+
     @include mq(tablet) {
       flex: 1;
     }
   }
+
   &__overview {
     max-width: 60ch;
     font-size: 16px;
