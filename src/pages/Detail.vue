8bcaa48f036bac8e3337be589b41d1a0f2bebcf1
Aydoğan Gökberk <aydogan.gokberk@gmail.com>
2021-07-18 16:15:33 +0300

    details hero css

--- src/pages/Detail.vue
+++ src/pages/Detail.vue
@@ -17,24 +17,30 @@
             :title="apiData.title"
           />
         </a>
-      </div>
-      <div class="hero__details">
-        <h1 class="hero__title">{{ apiData.title }}</h1>
-        <h4 class="hero__tagline">{{ apiData.tagline }}</h4>
         <div class="hero__row">
-          <span class="hero__rating">
-            <b>Rating:</b>
+          <span class="hero__rating" alt="Rating" title="Rating">
+            <b>⭐:</b>
             {{ apiData.vote_average }} / 10
           </span>
-          <span class="hero__runtime">
-            <b>Time:</b>
+          <span class="hero__runtime" alt="Rating" title="Time">
+            <b>⏱:</b>
             {{ apiData.runtime }} min
           </span>
         </div>
         <div class="hero__row">
-          <span class="hero__genre"></span>
+          <span
+            class="hero__genre"
+            v-for="genre in apiData.genres"
+            :key="genre.id"
+          >
+            {{ genre.name }}
+          </span>
         </div>
-        <p class="hero__overview"></p>
+      </div>
+      <div class="hero__details">
+        <h1 class="hero__title">{{ apiData.title }}</h1>
+        <h4 class="hero__tagline">"{{ apiData.tagline }}"</h4>
+        <p class="hero__overview">{{ apiData.overview }}</p>
       </div>
     </div>
   </section>
@@ -78,19 +84,84 @@ export default {
   background-size: cover;
   background-position: 50%;
   background-repeat: no-repeat;
-  height: 90vh;
+  min-height: 650px;
   padding: 2rem 1rem;
+  display: flex;
+  align-items: center;
+
+  &__poster {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    @include mq(tablet) {
+      align-items: flex-end;
+      width: 200px;
+    }
+  }
 
   &__info {
     display: flex;
     flex-direction: column;
     align-items: center;
+    gap: 1rem;
+
+    @include mq(tablet) {
+      flex-direction: row;
+      justify-content: center;
+      gap: 2rem;
+      align-items: flex-start;
+    }
   }
 
   &__row {
     display: flex;
     flex-direction: row;
-    gap: 1rem;
+    gap: 0.5rem;
+    flex-wrap: wrap;
+    margin: 0.5rem;
+    justify-content: center;
+
+    @include mq(tablet) {
+      justify-content: flex-end;
+    }
+  }
+
+  &__title {
+    font-size: 2rem;
+    margin: 0;
+    padding: 0;
+    text-shadow: #fff 1px 0 5px;
+
+    @include mq(tablet) {
+      font-size: 3rem;
+    }
+  }
+  &__tagline {
+    font-weight: 100;
+    font-style: italic;
+    margin: 0;
+  }
+  &__genre {
+    background: $lightbg;
+    padding: 0.5rem;
+    text-align: center;
+    @include mq(tablet) {
+      flex: 1;
+    }
+  }
+  &__overview {
+    max-width: 60ch;
+    font-size: 16px;
+    line-height: 26px;
+    word-wrap: break-word;
+    overflow-wrap: break-word;
+    hyphens: auto;
+
+    @include mq(tablet) {
+      font-size: 18px;
+      line-height: 30px;
+    }
   }
 }
 </style>
