0c1a329d801732d0f68c4f7e7af4b1c4fc9fe533
Aydoğan <aydogan.gokberk@gmail.com>
2021-09-03 12:55:49 +0300

    routing update

--- src/router.js
+++ src/router.js
@@ -2,22 +2,20 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import Home from "@/pages/Home.vue";
 
-const routes = [
-  {
-    path: "/",
-    name: "home",
-    component: Home,
-  },
-  {
-    path: "/detail/:id",
-    name: "detail",
-    component: () => import("@/pages/Detail.vue"),
-    props: true,
-  },
-];
-
 const router = createRouter({
   history: createWebHistory(),
-  routes,
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Home,
+    },
+    {
+      path: "/detail/:id",
+      name: "detail",
+      component: () => import("@/pages/Detail.vue"),
+      props: true,
+    },
+  ],
 });
 export default router;
